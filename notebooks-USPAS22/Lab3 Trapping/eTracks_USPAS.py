8372701734c7e314774d4b94ef92a7db8e773c5f
JiayangY <33234665+JiayangY@users.noreply.github.com>
2022-06-03 01:02:48 -0400

    Add files via upload

---
+++ notebooks-USPAS22/Lab3 Trapping/eTracks_USPAS.py
@@ -0,0 +1,178 @@
+import h5py as h5
+import numpy as np
+import math
+
+def getdata(id,path):
+    f = h5.File(path[id],"r")
+    datasetNames = [n for n in f.keys()] #Two Datasets: AXIS and e2
+    field = datasetNames[-1]
+    Field_dat = f[field][:].astype(float)
+
+    a1_bounds = f['AXIS']['AXIS1']
+    a2_bounds = f['AXIS']['AXIS2']
+
+    xi_dat = np.linspace(0,a1_bounds[1]-a1_bounds[0] ,len(Field_dat[0]))
+    r_dat = np.linspace(a2_bounds[0],a2_bounds[1],len(Field_dat))
+    return Field_dat, r_dat, xi_dat
+    
+
+def getAxes(path):
+    
+    den,r,xi = getdata(-1,path)
+    return r, xi
+
+def longE(path):
+    Ez, a,b = getdata(0,path)
+    return Ez
+
+def transE(path):
+    Er, a,b = getdata(1,path)
+    return Er
+
+def phiB(path):
+    Bphi, a,b = getdata(5,path)
+    return Bphi
+
+
+def Gamma(p):
+    return  math.sqrt(1 + p**2)
+def Velocity(pi,p):
+    v = pi / Gamma(p)
+    return v
+
+def find_nearest_index(array,value):
+    
+    idx = np.searchsorted(array, value, side="left")
+    if idx > 0 and (idx == len(array) or math.fabs(value - array[idx-1]) < math.fabs(value - array[idx])):
+        return idx-1
+    else:
+        return idx 
+    
+def EField(r,xi,axis,path):
+    # axis = 1 refers to xi-axis (longitudinal) field
+    # axis = 2 refers to r-axis (transverse) field
+    r_sim,xi_sim = getAxes(path)
+    Er_sim = transE(path)
+    Ez_sim = longE(path)
+    if axis == 2:
+        xiDex = find_nearest_index(xi_sim, xi)
+        rDex = find_nearest_index(r_sim, r)
+        return -Er_sim[rDex,xiDex]
+    elif axis == 1:
+        xiDex = find_nearest_index(xi_sim, xi)
+        rDex = find_nearest_index(r_sim, r)
+        return -Ez_sim[rDex, xiDex]
+
+def BForce(r,xi,v1,v2,axis,path):
+    r_sim,xi_sim = getAxes(path)
+    Bphi_sim = phiB(path)
+    xiDex = find_nearest_index(xi_sim, xi)
+    rDex = find_nearest_index(r_sim, r)
+    BField =  Bphi_sim[rDex, xiDex]
+    if axis == 1:
+        return -1.0 * v2 * BField
+    else:
+        return 1.0 * v1 * BField
+
+def Momentum(r, xi, dt, pr, pz,path):
+    p = math.sqrt(pr**2 + pz**2)
+    vr = Velocity(pr,p)
+    vz = Velocity(pz,p)
+
+    Fz = (EField(r, xi, 1,path) + BForce(r,xi,vz,vr,1,path))
+    Fr = (EField(r, xi, 2,path) + BForce(r,xi,vz,vr,2,path))
+ 
+    pz = pz + Fz * dt
+    pr = pr + Fr * dt
+    p = math.sqrt(pr**2 + pz**2)
+  
+    return pz, pr, p, Fr, Fz
+def GetTrajectory(path,r_0,xi_0,vph,dtw,step, prinit = 0, pzinit =0, t0 =0):
+   
+    r_dat, z_dat, t_dat, xi_dat, vz_dat,p_dat,pz_dat,pr_dat,F_dat, Fz_dat = np.array([]),np.array([]),np.array([]), np.array([]),np.array([]),np.array([]),np.array([]),np.array([]),np.array([]),np.array([])
+    trajectories = []
+    
+    r_sim,xi_sim = getAxes(path)
+    Er_sim = transE(path)
+    Ez_sim = longE(path)
+    Bphi_sim = phiB(path)
+
+    rn = r_0 # position in c/w_p
+    pr = prinit # momentum in m_e c
+    pz = pzinit
+    pinit = np.sqrt(prinit**2+pzinit**2)  
+    p = np.sqrt(pr**2+pz**2)
+
+    vrn = pr/Gamma(p) # velocity in c
+    t = t0 # start time in 1/w_p
+    dt = dtw # time step in 1/w_p
+
+    z0 = xi_0 + t0*vph
+    zn = xi_0 + t0*vph
+  
+    vzn = pz/Gamma(p)
+    dvz = 0.0
+    dvr = 0.0
+
+    old_r = r_0 #- 1.0
+    turnRad = r_0
+    xin = xi_0
+    
+
+   
+  #Iterate through position and time using a linear approximation
+  #until the radial position begins decreasing
+    i = 0 #iteration counter
+  # Iterate while electron energy is under 10GeV(100 MeV)
+ 
+    while Gamma(p) < pinit + 100 and i< int(step):
+        
+    #Determine Momentum and velocity at this time and position
+   
+        pz, pr, p, F, Fz = Momentum(rn, xin, dt, pr, pz,path)
+   
+        vzn = Velocity(pz,p)
+        vrn = Velocity(pr,p)
+ 
+    #Add former data points to the data lists
+        r_dat = np.append(r_dat, rn)
+        t_dat = np.append(t_dat, t)
+        z_dat = np.append(z_dat, zn)
+        vz_dat = np.append(vz_dat, vzn)
+        xi_dat = np.append(xi_dat, xin)
+        pr_dat = np.append(pr_dat, pr)
+        pz_dat = np.append(pz_dat, pz)
+        p_dat = np.append(p_dat, p)
+        F_dat = np.append(F_dat,F)
+        Fz_dat = np.append(Fz_dat, Fz)
+    #print("z = ", zn)
+        if rn > turnRad:
+            turnRad = rn
+
+    #print("vz=",vzn)
+    #Add the distance traveled in dt to r, increase t by dt
+        zn += vzn * dt
+        rn += vrn * dt
+        t += dt
+        xin = zn - vph*t
+        i += 1
+
+    # Allow for crossing the beam axis
+        #if rn < 0:
+        #    rn = -rn
+        #    pr = -pr
+        if rn > r_sim.max() or rn < r_sim.min() or xin < xi_sim.min() or xin > xi_sim.max():
+            
+            #print("out of box!")
+            
+            break
+    
+    xiPos = xin
+        
+    data = [xi_dat,r_dat,p_dat,pz_dat,pr_dat,F_dat, Fz_dat, z_dat, t_dat]
+ 
+      #trajectory.append([xi_dat,r_dat,p_dat,pz_dat,pr_dat,F_dat, Fz_dat, z_dat, t_dat])
+ 
+    del r_dat, xi_dat, z_dat, t_dat, pr_dat,pz_dat,F_dat, Fz_dat
+    return data
+    
---
+++ notebooks-USPAS22/Lab3 Trapping/laser_helper.py
@@ -0,0 +1,197 @@
+import sys
+b= sys.path
+sys.path=['/home/jovyan/analysis'] + b
+import osiris
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+from osh5vis import new_fig, osplot
+from osh5io import read_h5
+from osh5utils import fft
+import glob
+from ipywidgets import interact_manual,fixed,Layout,interact, FloatSlider
+from ipywidgets import interact, interactive, IntSlider, Layout, interact_manual, FloatSlider, HBox, VBox, interactive_output
+import ipywidgets as widgets
+interact_calc=interact_manual.options(manual_name="Make New Input and Run")
+import os
+import h5py as h5
+from osiris import tajima
+
+
+def newifile(iname='os-stdin', oname='output.txt', 
+             a0=4.0, omega0=36.0,w0 =4.0 , t_flat=0, t_rise=5.6, t_fall=5.6,
+             ndump=2,  tmax=200.0,nodex =1, nodez=1 ):
+
+    with open(iname) as osdata:
+        data = osdata.readlines()
+    nproc = nodex*nodez
+    for i in range(len(data)):
+        if 'node_number(1:2) =' in data[i]:
+      
+             data[i] = '    node_number(1:2) = '+str(nodez)+','+ str(nodex)+',\n'
+        if 'ndump =' in data[i]:
+            data[i] = '    ndump = '+str(ndump)+',\n'
+        if 'tmax =' in data[i]:
+            data[i] = '    tmax ='+str(tmax)+'\n'    
+        if 'a0 =' in data[i] and 'omega0' not in data[i]:
+            data[i] = '    a0= '+str(a0)+',\n'
+        if 'omega0 =' in data[i]:
+            data[i] = '    omega0 = '+str(omega0)+',\n'
+        if 'lon_flat =' in data[i]:
+            data[i] = '    lon_flat = '+str(t_flat)+',\n'
+        if 'lon_rise =' in data[i]:
+            data[i] = '    lon_rise = '+str(t_rise)+',\n'
+        if 'lon_fall =' in data[i]:
+            data[i] = '    lon_fall = '+str(t_fall)+',\n'
+        if 'per_w0 =' in data[i]:
+           
+            if str(oname[:-3]) =="linear":
+                data[i] = '    per_w0 = '+str(w0)+',\n'
+            if str(oname[:-3])=='nonlinear':
+                data[i] = '    per_w0 = '+str(2*np.sqrt(a0))+',\n'
+                print('The matched spot size W0 is', str(2*np.sqrt(a0)))
+        # This line is for moving window only
+      
+       
+
+    with open(oname,'w') as f:
+        for line in data:
+            f.write(line)
+    
+    print('New file '+oname+' is written.')
+    dirname = str(oname[:-3])
+    print(dirname)
+    print('Running OSIRIS in directory '+dirname+'...')
+    osiris.runosiris_2d(rundir=dirname,inputfile=oname, print_out='yes',combine='no',np=nproc)
+    if str(oname[:-3])=='nonlinear':    
+        print('The spot size W0 is adjusted to the matched spot size', str(2*np.sqrt(a0)))
+    
+    print('Done')
+ 
+def moving_widget(regime):
+    
+    style = {'description_width': '350px'}
+    layout = Layout(width='55%')
+    
+    if regime =='linear':
+        a0 = 0.5
+        w0 = 6
+        fname = 'linear.2d'
+    if regime == 'nonlinear':
+        a0 = 4.0
+        w0 = 2*np.sqrt(a0)
+        fname = 'nonlinear.2d'
+
+    a = widgets.Text(value='os-stdin', description='Template Input File:',style=style,layout=layout)
+    b = widgets.Text(value=fname, description='New Output File:',style=style,layout=layout)
+    #velocity
+   
+    # a0
+    d = widgets.FloatText(value=a0,description='a0:',style=style,layout=layout)
+    # frequency 
+    e = widgets.BoundedFloatText(value=10., min=0, max=50, description='omega0:',style=style,layout=layout)
+    # time 
+    f = widgets.BoundedFloatText(value=0, min=0, max=100, description='t_flat:',style=style,layout=layout)
+    g = widgets.BoundedFloatText(value=2.564152, min=0, max=100, description='t_rise:',style=style,layout=layout)
+    h = widgets.BoundedFloatText(value=2.564152, min=0, max=100, description='t_fall:',style=style,layout=layout)
+    
+    w0 = widgets.BoundedFloatText(value=w0, min=0.0, max=10.0, description='spot size W0:',style=style,layout=layout)
+    #xmaxw = widgets.FloatText(value=24, description='xmax:', style=style, layout=layout)
+    ndumpw = widgets.IntText(value=916, description='ndump:', style=style, layout=layout)
+    #ppc = widgets.IntText(value=2, min = 1, max = 3, description='Particles per cell:', style=style, layout=layout)
+    tmaxw = widgets.FloatText(value=40, max =200, description='tmax:', style=style, layout=layout)
+    #nx_pw = widgets.IntText(value=2400, description="Number of cells:", style=style, layout=layout)
+    
+    nodex = widgets.IntText(value=2, min=0, max=2,description="Nodes in x:", style=style, layout=layout)
+    nodez = widgets.IntText(value=2, min=0, max=2,description="Nodes in z:", style=style, layout=layout)
+
+    
+    im_moving = interact_calc(newifile, iname=a,oname=b,a0=d,omega0=e,w0=w0, t_flat=f, 
+                  t_rise=g, t_fall=h, ndump=ndumpw,  tmax=tmaxw,nodex=nodex,nodez=nodez)
+    im_moving.widget.manual_button.layout.width='250px'
+
+
+def getdata(id,path):
+    f = h5.File(path[id],"r")
+    datasetNames = [n for n in f.keys()] #Two Datasets: AXIS and e2
+    field = datasetNames[-1]
+    Field_dat = f[field][:].astype(float)
+
+    a1_bounds = f['AXIS']['AXIS1']
+    a2_bounds = f['AXIS']['AXIS2']
+
+    xi_dat = np.linspace(0,a1_bounds[1]-a1_bounds[0] ,len(Field_dat[0]))
+    r_dat = np.linspace(a2_bounds[0],a2_bounds[1],len(Field_dat))
+    return Field_dat, r_dat, xi_dat
+    
+def makeplot(file_id,path,field):
+    '''
+    def _den_plot(x_position,path):
+        den,r,xi = getdata(-1,path)
+        
+        rindex = np.searchsorted(r, x_position, side="left")
+        fig, ax = plt.subplots(figsize=(8,5))
+        
+        colors = ax.pcolormesh(xi,r,den,vmin=-2,vmax=0,cmap="Blues_r")
+ 
+        cbar = fig.colorbar(colors,ax=ax,pad = 0.15)
+        cbar.set_label('Density Field ($n_0$)')
+        
+        ax.hlines(r[rindex],xi[0],xi[-1],'k','--')
+        ax2 = ax.twinx()
+        ax2.plot(xi,den[rindex],'g',alpha = 0.7)
+        ax2.tick_params('y', colors='g')
+        ax.set_xlabel("$\\xi$ ($c/\omega_p$)")
+        ax.set_ylabel('r ($c/\omega_p$)')
+        ax.set_title('Density Field')
+    '''
+    def den_plot(path):
+        def _den_plot(x_position):
+            den,r,xi = getdata(-1,path)
+        
+            rindex = np.searchsorted(r, x_position, side="left")
+            fig, ax = plt.subplots(figsize=(8,5))
+        
+            colors = ax.pcolormesh(xi,r,den,vmin=-2,vmax=0,cmap="Blues_r")
+ 
+            cbar = fig.colorbar(colors,ax=ax,pad = 0.15)
+            cbar.set_label('Density Field ($n_0$)')
+        
+            ax.hlines(r[rindex],xi[0],xi[-1],'k','--')
+            ax2 = ax.twinx()
+            ax2.plot(xi,den[rindex],'g',alpha = 0.7)
+            ax2.tick_params('y', colors='g')
+            ax.set_xlabel("$\\xi$ ($c/\omega_p$)")
+            ax.set_ylabel('r ($c/\omega_p$)')
+            ax.set_title('Density Field')
+        return _den_plot
+
+
+    def field_plot(path):
+        def _field_plot(field_id,x_position):
+            id = int(field_id)
+            field,r,xi = getdata(id,path)
+            rindex = np.searchsorted(r, x_position, side="left")
+            fig, ax = plt.subplots(figsize=(8,5))
+    
+            colors = ax.pcolormesh(xi,r,field,vmin=-field.max(),vmax=field.max(),cmap="RdBu_r")
+    
+            cbar = fig.colorbar(colors,ax=ax,pad = 0.15)
+            name = ['Lonitudinal E', 'Transverse E', 'Transverse E','Longitudinal B','Transverse B','Transverse B','Psi' ]
+            
+            cbar.set_label(name[id] +' Field ($m_e c\omega_p / e$)')
+            ax.hlines(r[rindex],xi[0],xi[-1],'k','--') 
+            ax2 = ax.twinx()
+            ax2.plot(xi,field[rindex],'g',alpha = 0.7)
+            ax2.tick_params('y', colors='g')
+       
+            ax.set_xlabel("$\\xi$ ($c/\omega_p$)")
+            ax.set_ylabel('r ($c/\omega_p$)')
+            ax.set_title(name[id]+' Field')
+        return _field_plot
+  
+    if field == 'density':
+        interact(den_plot(path),x_position=FloatSlider(min=-5,max=5,step=0.05,continuous_update=False))
+    if field == 'field':
+        interact(field_plot(path),field_id = FloatSlider(min=0,max=6,step=1,continuous_update=False),x_position=FloatSlider(min=-5,max=5,step=0.05,continuous_update=False))
+    
